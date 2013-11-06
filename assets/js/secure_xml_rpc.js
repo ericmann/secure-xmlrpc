77516024e2a1019f891531d7a736d5e1e0249021
Eric Mann <eric@eamann.com>
2013-11-05 20:58:54 -0800

    First Grunt run

---
+++ assets/js/secure_xml_rpc.js
@@ -0,0 +1,8 @@
+/*! Secure XML-RPC - v0.1.0
+ * http://wordpress.org/plugins/secure-xmlrpc
+ * Copyright (c) 2013; * Licensed GPLv2+ */
+( function( window, undefined ) {
+	'use strict';
+
+
+} )( this );
